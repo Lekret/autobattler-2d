409afdab43042f654666ee89893c69bf78da8726
Lekret <lenicraft@mail.ru>
2022-09-25 00:12:21 +0300

    CharacterSelector

---
+++ Assets/Scripts/Services/CharacterSelector/ICharacterSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Logic;
+using Logic.Characters;
+
+namespace Services.CharacterSelector
+{
+    public interface ICharacterSelector
+    {
+        Character GetSingle(Team team);
+        void GetMany(Team team, int count, List<Character> buffer);
+        void GetManyDifferent(Team team, int count, List<Character> buffer);
+    }
+}
