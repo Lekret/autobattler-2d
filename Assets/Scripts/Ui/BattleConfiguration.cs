9884ded7f05f294b5bd604a0d516086d2a3cb66f
Lekret <lenicraft@mail.ru>
2022-09-24 09:41:52 +0300

    BattleConfiguration WIP

---
+++ Assets/Scripts/Ui/BattleConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Services.StaticData;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace Ui
+{
+    public class BattleConfiguration : MonoBehaviour
+    {
+        public Button StartButton;
+        public GameObject SelectionRoot;
+        
+        private void Construct(ICharacterDataService characterDataService)
+        {
+            foreach (var data in characterDataService.GetAll())
+            {
+                
+            }
+        }
+        
+        private void Awake()
+        {
+            StartButton.onClick.AddListener(StartBattle);
+        }
+
+        private void OnDestroy()
+        {
+            StartButton.onClick.RemoveListener(StartBattle);
+        }
+
+        private void StartBattle()
+        {
+            Destroy(this);
+        }
+    }
+}
