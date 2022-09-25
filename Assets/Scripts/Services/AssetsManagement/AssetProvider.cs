50bf78c50023fe0c7f20597eecafaafcae7c0e43
Lekret <lenicraft@mail.ru>
2022-09-25 09:14:13 +0300

    AssetData

--- Assets/Scripts/Services/AssetsManagement/AssetProvider.cs
+++ Assets/Scripts/Services/AssetsManagement/AssetProvider.cs
@@ -33,7 +33,7 @@ namespace Services.AssetsManagement
             {
                 foreach (var handle in resourceHandles)
                 {
-                    // Addressables.Release(handle);
+                    Addressables.Release(handle);
                 }
             }
         }
