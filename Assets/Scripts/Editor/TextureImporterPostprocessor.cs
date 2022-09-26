40095917194168f7288ced9488b13a9d1de380d7
Lekret <lenicraft@mail.ru>
2022-09-26 04:43:12 +0300

    Skip plugins postprocess

--- Assets/Scripts/Editor/TextureImporterPostprocessor.cs
+++ Assets/Scripts/Editor/TextureImporterPostprocessor.cs
@@ -8,6 +8,8 @@ namespace Editor
         private void OnPostprocessTexture(Texture2D texture)
         {
             var importer = (TextureImporter) assetImporter;
+            if (importer.assetPath.Contains("Assets/Plugins"))
+                return;
             importer.spritePixelsPerUnit = 32;
             importer.filterMode = FilterMode.Point;
         }
