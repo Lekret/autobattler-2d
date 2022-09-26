c63aaf5f8068abe03e3a4717a72ffccccdcdd77e
Lekret <lenicraft@mail.ru>
2022-09-26 04:40:48 +0300

    TextureImporterPostprocessor

---
+++ Assets/Scripts/Editor/TextureImporterPostprocessor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class TextureImporterPostprocessor : AssetPostprocessor
+    {
+        private void OnPostprocessTexture(Texture2D texture)
+        {
+            var importer = (TextureImporter) assetImporter;
+            importer.spritePixelsPerUnit = 32;
+            importer.filterMode = FilterMode.Point;
+        }
+    }
+}
